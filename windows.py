4bcc343430a75653f278f388bb16e17942d0ba86
叶骋 <yc20020611@mail.ustc.edu.cn>
2023-10-10 15:50:23 +0800

    补全了各个按钮的功能，实现了绘图按钮，基本完成了整个项目的设计

--- windows.py
+++ windows.py
@@ -6,14 +6,12 @@ window = tk.Tk()
 countrylist = data_gdp['GDP, current prices (Billions of U.S. dollars)'][1:229].tolist()  # 国家列表
 indexmap = {'GDP': 'gdp', '人口': 'renkou'}
 indexlist = list(indexmap)
-typemap = {'线形图': 'line', '柱状图': 'bar'}
 window.title('经济指标曲线图')
 w_width = 1100
 w_height = 750
 scn_width = window.maxsize()[0]
 x_point = (scn_width - w_width) // 2
 window.geometry('%dx%d+%d+%d' % (w_width, w_height, x_point, 100))
-window.wm_attributes('-topmost', True)
 window.tk_focusFollowsMouse()
 window.bind("<Escape>", lambda event: window.iconify())
 frame0 = tk.Frame(window, pady=10, padx=15)
@@ -32,6 +30,37 @@ def selectall():
         v[index].set(item)
 
 
+def get_plt():
+    index = var.get()  # 经济指标
+    if index == 'GDP':
+        data = data_gdp
+        name = 'GDP, current prices (Billions of U.S. dollars)'
+    elif index == '人口':
+        data = data_population
+        name = 'Population (Millions of people)'
+    chosed = [i.get() for i in v if i.get()]
+    startyear = start_year.get()
+    if (not startyear):
+        messagebox.showerror('警告', '没有起始年份')
+        return False
+    endyear = end_year.get()
+    if (not endyear):
+        messagebox.showerror('警告', '没有结束年份')
+        return False
+    endyear = str(int(endyear) + 1)
+    year_label = [i for i in range(int(startyear), int(endyear))]
+    for country in chosed:
+        start_index = int(startyear) - 1980
+        end_index = int(endyear) - 1980
+        data_x = np.array(data.loc[data[name] == country].iloc[:, start_index + 1:end_index + 1]).flatten()
+        # print(data_x)
+        plt.plot(year_label, data_x, label=country)
+        plt.xlabel("year")
+        plt.ylabel(index)
+        plt.legend(loc="best")
+    plt.show()
+
+
 frame1 = tk.Frame(window, pady=6, padx=15)
 frame1.grid(row=1, column=0)
 opt = tk.IntVar()
@@ -42,6 +71,7 @@ for index, item in enumerate(countrylist):
                                                                                        column=index % 7, sticky='w')
 ttk.Radiobutton(frame1, text='全选', variable=opt, value=1, command=selectall).grid(row=0, column=0, sticky='w')
 ttk.Radiobutton(frame1, text='反选', variable=opt, value=0, command=unselectall).grid(row=0, column=1, sticky='w')
+
 frame2 = tk.Frame(window, padx=15, pady=15)
 frame2.grid(row=2, column=0, sticky='w')
 var = tk.StringVar()
@@ -51,12 +81,6 @@ chosen = ttk.Combobox(frame2, textvariable=var)
 chosen.grid(row=0, column=1, sticky='w')
 chosen['values'] = indexlist
 chosen.current(0)
-
-ttk.Label(frame2, text="请选择图表类型:").grid(row=0, column=2, sticky='w')
-chosen = ttk.Combobox(frame2, textvariable=type)
-chosen.grid(row=0, column=3, sticky='w')
-chosen['values'] = ['线形图', '柱状图']
-chosen.current(0)  # 默认是第一个
 
 start_year = tk.StringVar()
 end_year = tk.StringVar()
