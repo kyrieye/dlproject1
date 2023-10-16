90bfc48a0ae124615106d2810222f2db2e5e0124
叶骋 <yc20020611@mail.ustc.edu.cn>
2023-10-16 10:08:36 +0800

    程序主入口

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+'''Due to the requirements for crawling data, please be careful to turn off the VPN when running the program'''
 from windows import Windows
 
-windows = Windows()
-windows.build_windows()
-windows.start()
+windows = Windows()  # Create instance
+windows.build_windows()  # initialization window
+windows.start()  # Start window
