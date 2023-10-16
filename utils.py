7506ec3ddc758714228ec61df7fb6e36aec687a6
叶骋 <yc20020611@mail.ustc.edu.cn>
2023-10-16 10:08:50 +0800

    获取数据文件

--- utils.py
+++ utils.py
@@ -1,17 +1,12 @@
+import logging
 import requests
 import pandas as pd
-import logging
-import requests, time, datetime
-import tkinter as tk  # 使用Tkinter前需要先导入
-from tkinter import filedialog, messagebox, ttk
-import pandas as pd
 import numpy as np
-import matplotlib.pyplot as plt
-from sqlalchemy import create_engine
 
 logging.captureWarnings(True)
 
 
+# Use the request class to crawl data
 def get_data():
     url = 'https://www.imf.org/external/datamapper//export/excel.php?indicator=NGDPD'
     response = requests.get(url, verify=False)
@@ -22,22 +17,19 @@ def get_data():
     open("Population.xls", 'wb').write(response.content)
     data_popu = pd.read_excel("Population.xls")
     index = data_gdp.columns[0]
-    data_gdp.dropna(thresh=1, inplace=True)  # 去空行
-    data_gdp.set_index('GDP, current prices (Billions of U.S. dollars)', inplace=True)  # 国家列为索引
-    data_gdp.dropna(thresh=1, inplace=True)  # 去最后一行
+    data_gdp.dropna(thresh=1, inplace=True)  # Remove blank lines
+    data_gdp.set_index('GDP, current prices (Billions of U.S. dollars)', inplace=True)  # Index the ’GDP‘ column
+    data_gdp.dropna(thresh=1, inplace=True)  # Remove the last line
     for i in data_gdp.columns:
-        data_gdp[i].mask(data_gdp[i] == 'no data', np.nan, inplace=True)  # 处理no data
-    data_gdp = data_gdp.astype(float).reset_index()  # 数据转为float
-    #print(data_gdp)
+        data_gdp[i].mask(data_gdp[i] == 'no data', np.nan, inplace=True)  # Process the no data
+    data_gdp = data_gdp.astype(float).reset_index()  # Convert data to float type
+    # print(data_gdp)
     index = data_popu.columns[0]
-    data_popu.dropna(thresh=1, inplace=True)  # 去空行
-    data_popu.set_index('Population (Millions of people)', inplace=True)  # 国家列为索引
-    data_popu.dropna(thresh=1, inplace=True)  # 去最后一行
+    data_popu.dropna(thresh=1, inplace=True)  # Remove blank lines
+    data_popu.set_index('Population (Millions of people)', inplace=True)  # Index the ’Population‘ column
+    data_popu.dropna(thresh=1, inplace=True)  # Remove the last line
     for i in data_popu.columns:
-        data_popu[i].mask(data_popu[i] == 'no data', np.nan, inplace=True)  # 处理no data
-    data_popu = data_popu.astype(float).reset_index()  # 数据转为float
-    #print(data_popu)
+        data_popu[i].mask(data_popu[i] == 'no data', np.nan, inplace=True)  # Process the no data
+    data_popu = data_popu.astype(float).reset_index()  # Convert data to float type
+    # print(data_popu)
     return data_gdp, data_popu
-
-
-get_data()
